6a46291d8cac502050cc02cde34e4228e15c3bd1
Nikita Davidchuk <bizy18588@gmail.com>
2022-12-03 00:47:53 +0300

    added wsgi server
    Signed-off-by: Nikita Davidchuk <bizy18588@gmail.com>

--- run_server.py
+++ run_server.py
@@ -3,9 +3,16 @@ from bot import EchoBot
 import os
 from schemes import engine
 from flask_cors import CORS
+from wsgiserver import WSGIServer
 
 if __name__ == '__main__':
     bot = EchoBot(engine)
     webserver = WebServer(__name__, engine, bot)
     CORS(webserver, resources={r"/*": {"origins": "*"}})
-    webserver.run(host=os.getenv('FLASK_HOST'), port=os.getenv('FLASK_PORT'), debug=os.getenv('FLASK_DEBUG'))
+
+    if os.getenv('FLAST_DEBUG'):
+        webserver.run(host=os.getenv('FLASK_HOST'), port=os.getenv('FLASK_PORT'), debug=True)
+    
+    else:
+        server = WSGIServer(webserver, host=os.getenv('FLASK_HOST'), port=int(os.getenv('FLASK_PORT')))
+        server.start()
